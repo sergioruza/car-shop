7938ebdef12ed7becc071e273aec60ed66ba65b9
Sergio <sergioruza22@gmail.com>
2023-03-22 20:47:24 -0300

    tests: /cars

---
+++ tests/unit/Services/Cars.test.ts
@@ -0,0 +1,111 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { Model, isValidObjectId } from 'mongoose';
+import Car from '../../../src/Domains/Car';
+import CarService from '../../../src/Services/CarService';
+
+describe('Testa todas as rotas de /cars', function () {
+  it('Cria um carro com sucesso', async function () {
+    const input = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const output: Car = new Car({
+      id: '641b83f73816f4e756286444',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    });
+
+    sinon.stub(Model, 'create').resolves(output);
+    const service = new CarService();
+    const result = await service.createCar(input);
+    expect(result).to.be.deep.equal(output);
+  });
+
+  it('Busca todos os carros com sucesso', async function () {
+    const output = [
+      {
+        id: '634852326b35b59438fbea2f',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+      {
+        id: '634852326b35b59438fbea31',
+        model: 'Tempra',
+        year: 1995,
+        color: 'Black',
+        status: false,
+        buyValue: 39,
+        doorsQty: 2,
+        seatsQty: 5,
+      },
+    ];
+
+    sinon.stub(Model, 'find').resolves(output);
+
+    const service = new CarService();
+    const result = await service.findAll();
+    expect(result).to.be.deep.equal(output);
+  });
+
+  it('Erro em caso de não achar nenhum carro', async function () {
+    sinon.stub(Model, 'find').resolves([]);
+
+    try {
+      const service = new CarService();
+      await service.findAll();
+    } catch (err) {
+      expect((err as Error).message).to.be.equal('Car not found');
+    }
+  });
+
+  it('Lança um erro ao pesquisar um carro com id inválido', async function () {
+    sinon.stub(Model, 'findById').resolves(undefined);
+
+    try {
+      const service = new CarService();
+      await service.findById('46548647888678');
+    } catch (err) {
+      expect((err as Error).message).to.be.equal('Invalid mongo id');
+    }
+  });
+
+  it('Trás um carro buscado por id', async function () {
+    const output = {
+      id: '641b83f73816f4e756286444',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    sinon.stub(isValidObjectId);
+    sinon.stub(Model, 'findById').resolves(output);
+
+    const service = new CarService();
+    const result = await service.findById('641b83f73816f4e756286444');
+    expect(result).to.be.equal(output);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
